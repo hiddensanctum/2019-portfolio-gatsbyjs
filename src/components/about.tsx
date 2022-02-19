ca283c8a7aa4da843f9798f8201566f4aa65a3b5
James C <5080145+hiddensanctum@users.noreply.github.com>
2022-02-19 13:12:28 -0800

    Fix typo

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -14,8 +14,7 @@ const About:FunctionComponent = () => (
         <h2>About Me</h2>
         <h3>I am a Senior Frontend Engineer in Seattle, Washington</h3>
         <p>Studied at Code Fellows RoR Track and UMCP Bachelors of Science in Psychology,
-          I predominately focus on Front End Stacks tackling issues of Accessibility, D
-          esign Systems, and User Experiences.</p>
+          I predominately focus on Front End Stacks tackling issues of Accessibility, Design Systems, and User Experiences.</p>
       </div>
       <div className='skill-section' data-aos='fade-down'>
         <Skills />
