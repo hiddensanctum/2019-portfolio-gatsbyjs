e4bf948294e68aed49319a436e8bf7b6fddc42f6
James C <5080145+hiddensanctum@users.noreply.github.com>
2022-02-19 13:12:27 -0800

    fix mobile navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ import "../styles/navbar.scss"
 const Navbar = () => {
   const [MobileNav, toggleMobileNav] = useState(false);
 
-  const navBarContent = (device) => {device === "navbar-mobile" ? (
+  const navBarContent = (device) => device === "navbar-mobile" ? (
     <ul className={device}>
       <li><Link to={`/`} onClick={() => toggleMobileNav(!MobileNav)}>Home</Link></li>
       <li><Link to={`#about`} onClick={() => toggleMobileNav(!MobileNav)}>About</Link></li>
@@ -16,8 +16,7 @@ const Navbar = () => {
       <li><Link to={`#journal`} onClick={() => toggleMobileNav(!MobileNav)}>Journal</Link></li>
       <li><Link to={`#contact`} onClick={() => toggleMobileNav(!MobileNav)}>Contact</Link></li>
     </ul>
-  )
-  : (
+  ) : (
     <ul className={device}>
       <li><Link to={`/`}>Home</Link></li>
       <li><Link to={`#about`}>About</Link></li>
@@ -26,7 +25,7 @@ const Navbar = () => {
       <li><Link to={`#journal`}>Journal</Link></li>
       <li><Link to={`#contact`}>Contact</Link></li>
     </ul>
-  )};
+  );
 
   return (
     <nav className="navbar">
