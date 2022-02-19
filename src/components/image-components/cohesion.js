027d78304d3b45996e8171633357517b2af3db97
James C <5080145+hiddensanctum@users.noreply.github.com>
2022-02-19 13:12:27 -0800

    update projects section

---
+++ src/components/image-components/cohesion.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { StaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';  
+
+const CohesionImage = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query CohesionPhotoQuery {
+          allImageSharp(filter: {fluid: {originalName: {eq: "cohesion.png"}}}) {
+            edges {
+              node {
+                fluid {
+                  base64
+                  tracedSVG
+                  srcWebp
+                  srcSetWebp
+                  originalImg
+                  originalName
+                  presentationWidth
+                  presentationHeight
+                }
+              }
+            }
+          }
+        }         
+      `}
+      render={data => {
+        const photo = data.allImageSharp.edges[0].node.fluid;
+        return (
+          <div>
+            <Image fluid={photo} className="project-image"/>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+export default CohesionImage
---
+++ src/components/image-components/sdot.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { StaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';  
+
+const SdotImage = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query SdotPhotoQuery {
+          allImageSharp(filter: {fluid: {originalName: {eq: "SDOT.png"}}}) {
+            edges {
+              node {
+                fluid {
+                  base64
+                  tracedSVG
+                  srcWebp
+                  srcSetWebp
+                  originalImg
+                  originalName
+                  presentationWidth
+                  presentationHeight
+                }
+              }
+            }
+          }
+        }         
+      `}
+      render={data => {
+        const photo = data.allImageSharp.edges[0].node.fluid;
+        return (
+          <div>
+            <Image fluid={photo} className="project-image"/>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+export default SdotImage
