7dd763dcaebda4a0030977523767905325bfe061
James C <5080145+hiddensanctum@users.noreply.github.com>
2022-02-19 13:12:28 -0800

    Fix typos and spelling

--- src/components/experience.js
+++ src/components/experience.js
@@ -14,26 +14,19 @@ const Experience = () => (
           <span>May 2018 - Present</span>
           <h3>Tyler Technologies</h3>
           <h4>Senior Software Engineer</h4>
-          <p>Currently I work as a Senior Software Engineer in the Data Insights Division of Tyler
-            Technologies developing Performance KPI software to enable data analysis for government
-            data to drive data based decisions.</p>
+          <p>Currently, I work as a Senior Software Engineer at the Data Insights Division of Tyler Technologies. I develop Performance KPI software that enables data analysis for governments to make data-based decisions for their public policies.</p>
         </div>
         <div className='experience-block'>
           <span>Apr 2014 - May 2018</span>
           <h3>Socrata</h3>
           <h4>Senior Front End Developer</h4>
-          <p>Scope out large data exploration and story telling applications for the public sector.
-            Provide mentorship for newer developers. Develop all-inclusive technical project plan
-            and excute on that project plan. Create the UI/UX design and worked with program manager
-            to develop roadmaps.</p>
+          <p>Scope out large data exploration and storytelling applications for the public sector. Provide mentorship for newer developers. Develop an all-inclusive technical project plan and execute on that project plan. Create the UI/UX design and worked with the program manager to develop roadmaps.</p>
         </div>
         <div className='experience-block'>
           <span>Jan 2014 - Mar 2014</span>
           <h3>Socrata</h3>
-          <h4>Constultant Web Developer</h4>
-          <p>Volunteered as a consultant web developer and developed an Ember application to track
-            feral cat populations in Seattle. The Ember application is designed to be a tool to help
-            volunterrs do data entry and track the feral cat population.</p>
+          <h4>Consultant Web Developer</h4>
+          <p>Volunteered as a consultant web developer and developed an Ember application to track feral cat populations in Seattle. The Ember application was a tool to help volunteers perform data entry and track the feral cat population.</p>
         </div>
       </div>
       <div className='education' data-aos='fade-down'>
@@ -44,9 +37,7 @@ const Experience = () => (
           <span>Oct 2013 - Dec 2013</span>
           <h3>Code Fellows</h3>
           <h4>Ruby on Rails</h4>
-          <p>Entrance competitive immersive coding bootcamp taught by industry practitioners in the
-            heart of Seattle’s tech community. Code Fellows is designed to take high-potential
-            individuals with moderate coding experience and turn them into great developers.</p>
+          <p>Entrance competitive immersive Coding Bootcamp taught by industry practitioners in the heart of Seattle’s tech community. Code Fellows takes high-potential individuals with moderate coding experience and turn them into great developers.</p>
         </div>
         <div className='education-block'>
           <span>Sep 2008 - May 2012</span>
