fc2348ae24a1db8c5b193f5992ed1d0d01270920
James C <5080145+hiddensanctum@users.noreply.github.com>
2022-02-19 13:12:28 -0800

    Speed up background loading

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import { graphql, StaticQuery } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 import '../styles/landing.scss';
 
 const Landing = () => (
-  <div className='landing-section'>
-    <div className='landing-layout'>
-      <a href='#about' className='anchor'>
-        <div>
-          <h1 className='intro'>James Chuang</h1>
-          <p className='subtitle'>Senior Software Engineer | Seattle, Washington</p>
+
+
+<StaticQuery
+  query={graphql`
+    query {
+      desktop: file(relativePath: { eq: "landing-background.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `}
+  render={data => {
+    const imageData = data.desktop.childImageSharp.fluid
+    return (
+      <BackgroundImage
+        Tag="section"
+        className='landing-section'
+        fluid={imageData}
+      >
+        <div className='landing-layout'>
+          <a href='#about' className='anchor'>
+            <div>
+              <h1 className='intro'>James Chuang</h1>
+              <p className='subtitle'>Senior Software Engineer | Seattle, Washington</p>
+            </div>
+          </a>
         </div>
-      </a>
-    </div>
-  </div>
+      </BackgroundImage>
+    )
+  }}
+/>
+
 );
 
 export default Landing;
